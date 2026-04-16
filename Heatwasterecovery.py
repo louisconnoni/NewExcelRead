61363f4f78d0a602f92705f39993458f596eb6b5
louisconnoni <louis.connoni@gmail.com>
2026-04-16 15:03:57 -0400

    Update Heatwasterecovery.py

--- Heatwasterecovery.py
+++ Heatwasterecovery.py
@@ -77,7 +77,7 @@ if uploaded_file:
                     "Select metrics to display",
                     [col for col in results_df.columns if col != "Scenario"],
                     default=["Carbon Score", "Economic Score", "Water Score", "Social Score"],
-                    help = "Carbon Weighting"+5
+                    help = "Carbon Weighting"+"Cheese"
                 )
                 
                 if metrics:
