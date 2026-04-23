4bf5b0335796ef0e0d921f0544b051ef9f72c870
louisconnoni <louis.connoni@gmail.com>
2026-04-23 12:35:12 -0400

    Update Desalination.py

--- Desalination.py
+++ Desalination.py
@@ -1,1 +1,40 @@
 
+import matplotlib.pyplot as plt
+import math as math
+import numpy as np
+import cmath as cmath
+import pandas as pd
+import numpy_financial as nf
+
+# model.py
+
+import numpy as np
+import math
+import numpy_financial as nf
+
+
+def run_model_for_column(op):
+
+ #-
+ ## Razas portions
+
+ return {
+  "Total Profit": totalprofit,
+  "Total Carbon Saved": totalcarbonsaved,
+  "Total Water Saved": totalwatersaved,
+  "Total Score" : totalscore,
+  "Carbon Score": carbonscore,
+  "Water Score": waterscore,
+  "Economic Score": econscore,
+  "Social Score": socialscore,
+  "ERE improvement": EREpercent*-1,
+  "ERF" : ERF,
+  "Error": errors[0],
+  "Economic Weight":Se,
+  "Water Weight":Sw,
+  "Carbon Weight":Sc,
+  "Social Weight":Ssocial
+    
+      
+        
+    }
