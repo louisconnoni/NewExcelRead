ba218b8f9d3dda0ba89beae97695a3be9109b5d6
louisconnoni <louis.connoni@gmail.com>
2026-04-30 14:47:52 -0400

    Update Desalination.py

--- Desalination.py
+++ Desalination.py
@@ -532,6 +532,11 @@ def run_desalination_model(op):
   "Water Weight":Sw,
   "Carbon Weight":Sc,
   "Social Weight":Ssocial
+  "Pipe Cost":pipeweight*pipppw,
+  "Pump Cost":100*60*(pumppower/373)**0.8,
+  "Heat Exchanger Cost":3200*(wht/100000)**0.6,
+  "Maintenance per annum":maintenancecost * (wht/1000000),
+  "Electricity per annum":electricity
     
       
         
